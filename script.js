1c1180099df0114e38d77d9f512dc4c620fa883c
vanshaj-online <79051874+vanshaj-online@users.noreply.github.com>
2024-02-07 17:04:21 +0530

    completed till section4

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 let stripe = document.querySelector('.stripe');
 let nav = document.querySelector('nav')
 let header = document.querySelector('header')
+let prevBtn = document.querySelector('[data-left-btn]')
+let nextBtn = document.querySelector('[data-right-btn]')
+let slides = document.querySelectorAll('.project-section-slides')
+let counter = 0;
+
+
+prevBtn.addEventListener('click', () => {
+    slides.forEach((e) =>{
+         e.scrollBy({
+             left : -27 * 16,
+             behavior: "smooth"
+         });
+     })
+})
+nextBtn.addEventListener('click', () => {
+    slides.forEach((e) =>{
+         e.scrollBy({
+             left : 27 * 16,
+             behavior: "smooth"
+         });
+     })
+})
+
+
 
 const observer = new IntersectionObserver((entry) =>{
     entry.forEach((elem) =>{
@@ -9,7 +33,6 @@ const observer = new IntersectionObserver((entry) =>{
         if(!elem.intersectionRatio == 1){
             nav.style.position = 'fixed'
             nav.style.transform = 'translatey(-30px)'
-            console.log('hey')
         }
     })
 })
